2d38208a6f9b3eca11e33af8723a5cba08814ed3
Vasilii <jud3dx@gmail.com>
2021-03-12 17:02:32 +0500

    apply cycles

--- script.js
+++ script.js
@@ -10,12 +10,27 @@ const personalMoveDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = prompt('На сколько оцените его?', ''),
-  c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-personalMoveDB.movies[a] = b;
-personalMoveDB.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMoveDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMoveDB.count < 10) {
+  console.log('Посмотренно довольно мало фильмов');
+} else if (personalMoveDB.count >= 10 && personalMoveDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMoveDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
 console.log(personalMoveDB);
